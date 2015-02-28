0f7ae9fa1d8b6ab934b3c9f576a496969f9eadfb
KwonMin Jung <manjonging@naver.com>
2015-03-01 04:03:31 +0900

    no message

---
+++ js/index.js
@@ -0,0 +1,17 @@
+$(function(){
+    $(".about_menu").click( function() {
+        showAboutSlide();
+    });
+    
+    function showAboutSlide() {
+        $(".about_slide_container").css("left", "230px");
+    }
+});
+
+$(document).ready(function() {
+    
+});
+
+
+
+

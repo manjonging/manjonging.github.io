090a382a47f1355dcedf63c7872c568c81e1bebb
KwonMin Jung <manjonging@naver.com>
2015-03-01 05:49:51 +0900

    no message

--- js/index.js
+++ js/index.js
@@ -1,16 +1,37 @@
 $(function(){
     $(".about_menu").click( function() {
-        showAboutSlide();
+        $(".about_slide_container").css("left", "230px");
+        
+        $(".contact_slide_container").css("left", "-100%");
+    });
+    $(".contact_menu").click( function() {
+        $(".about_slide_container").css("left", "-100%");
+        $(".contact_slide_container").css("left", "230px");
     });
     
-    function showAboutSlide() {
-        $(".about_slide_container").css("left", "230px");
-    }
+    $(".about_slide_container .x-sign").click(function() {
+        $(".about_slide_container").css("left", "-100%");
+        
+    });
+    $(".contact_slide_container .x-sign").click(function() {
+        
+        $(".contact_slide_container").css("left", "-100%");
+    });
+    
+   
 });
 
 $(document).ready(function() {
     
 });
+
+
+
+$(document).ready(function() {
+    
+});
+
+
 
 
 
